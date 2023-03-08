64bf22d58c6e81c0f93e36e342bf51b3c528fafe
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-08 15:14:06 +0100

    Displays all added reviews on #show

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants do
-    resources :reviews, only: [:new]
+    resources :reviews, only: [:new, :create]
   end
 end
