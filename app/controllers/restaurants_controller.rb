65a7a6936ea452902eaefcf24ba2c95bd10b6e03
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-08 15:35:16 +0100

    Uses simple form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,7 +14,7 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
-    redirect_to restaurant_path(@restaurant)
+    redirect_to restaurants_path(@restaurant)
   end
 
   private
