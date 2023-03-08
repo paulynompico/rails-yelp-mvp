a6de99ea7342385369b68025499dcbc95c3153ca
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-03-08 13:27:43 +0100

    Added review model with validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,5 @@ class Restaurant < ApplicationRecord
 
   validates :name, :address, :phone_number, :category, presence: true
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], message: "%{value} is not a valid category" }
+  validates :phone_number, format: { with: /\A(\d{2}\s?){5}\z/ }
 end
